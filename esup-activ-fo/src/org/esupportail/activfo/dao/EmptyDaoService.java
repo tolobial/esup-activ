229eb1d84e595b8ad769a0a7e7fa24da61778fcf
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-06-10 15:54:52 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@194 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/dao/EmptyDaoService.java
@@ -0,0 +1,65 @@
+package org.esupportail.activfo.dao;
+
+import java.util.List;
+
+import org.esupportail.activfo.domain.beans.User;
+import org.esupportail.activfo.domain.beans.VersionManager;
+import org.esupportail.commons.web.beans.Paginator;
+
+public class EmptyDaoService implements DaoService {
+
+	private static final long serialVersionUID = 3429875756876395115L;
+
+	
+	
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addVersionManager(VersionManager versionManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Paginator<User> getAdminPaginator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUser(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<User> getUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<VersionManager> getVersionManagers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateVersionManager(VersionManager versionManager) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public VersionManager getVersionManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
