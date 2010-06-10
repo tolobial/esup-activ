0d29616a0ed410a06715ad54a5546658ed39378b
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-06-10 15:54:33 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@193 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/domain/DomainServiceImpl.java
+++ esup-activ-fo/src/org/esupportail/activfo/domain/DomainServiceImpl.java
@@ -4,14 +4,26 @@
  */
 package org.esupportail.activfo.domain;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.esupportail.activfo.dao.DaoService;
+import org.esupportail.activfo.domain.beans.Account;
 import org.esupportail.activfo.domain.beans.User;
 import org.esupportail.activfo.domain.beans.VersionManager;
+import org.esupportail.activfo.domain.tools.StringTools;
+import org.esupportail.activfo.services.ldap.InvalidLdapAccountException;
+import org.esupportail.activfo.services.ldap.LdapSchema;
+import org.esupportail.activfo.services.ldap.NotUniqueLdapAccountException;
+import org.esupportail.activfo.services.remote.Information;
 import org.esupportail.commons.exceptions.ConfigException;
 import org.esupportail.commons.exceptions.UserNotFoundException;
 import org.esupportail.commons.services.application.Version;
+import org.esupportail.commons.services.ldap.LdapException;
 import org.esupportail.commons.services.ldap.LdapUser;
 import org.esupportail.commons.services.ldap.LdapUserService;
 import org.esupportail.commons.services.logging.Logger;
@@ -32,6 +44,8 @@ public class DomainServiceImpl implements DomainService, InitializingBean {
 	 */
 	private static final long serialVersionUID = -8200845058340254019L;
 
+	private Information service;
+	
 	/**
 	 * {@link DaoService}.
 	 */
@@ -71,6 +85,13 @@ public class DomainServiceImpl implements DomainService, InitializingBean {
 				"property displayNameLdapAttribute of class " + this.getClass().getName() 
 				+ " can not be null");
 	}
+	
+	
+	
+	
+	
+	
+	
 
 	//////////////////////////////////////////////////////////////
 	// User
@@ -265,5 +286,155 @@ public class DomainServiceImpl implements DomainService, InitializingBean {
 	public void setLdapUserService(final LdapUserService ldapUserService) {
 		this.ldapUserService = ldapUserService;
 	}
+	
+	
+	public boolean validateAccount(Account account) throws LdapException {
+
+		try {
+			System.out.println(account.getHarpegeNumber());
+			System.out.println("account1");
+			if (this.ldapUserService.getClass()==null){
+				System.out.println("kkkkkkkkkkk");
+			}
+			List<LdapUser> ldapUserList = this.ldapUserService
+					.getLdapUsersFromFilter("(supannEmpId="
+							+ account.getHarpegeNumber() + ")");
+			System.out.println("account2");
+			if (ldapUserList.size() > 1) {
+				throw new NotUniqueLdapAccountException(
+						"LDAP account with supannEmpId "
+								+ account.getHarpegeNumber() + " not unique");
+			} else if (ldapUserList.size() == 0) {
+				return false;
+			}
+
+			LdapUser ldapUser = ldapUserList.get(0);
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("Validating account for : " + ldapUser);
+				logger.debug("Birthdate checking");
+			}
+
+			/*
+			 * Birthdate checking
+			 */
+
+			String ldapUserBirthdateStr = ldapUser.getAttribute(LdapSchema
+					.getBirthdate());
+
+			if (ldapUserBirthdateStr == null) {
+				String errmsg = "LDAP account with supannEmpId "
+						+ account.getHarpegeNumber() + " has no birthdate";
+				logger.error(errmsg);
+				throw new InvalidLdapAccountException(errmsg);
+			}
+
+			Date ldapUserBirthdate;
+
+			try {
+				SimpleDateFormat formatter = new SimpleDateFormat(LdapSchema
+						.getBirthdateFormat());
+
+				/*
+				 * TimeZone must be the same in user interface and LDAP, UTC is
+				 * chosen
+				 */
+				TimeZone tz = TimeZone.getTimeZone("UTC");
+				formatter.setTimeZone(tz);
+
+				ldapUserBirthdate = formatter.parse(ldapUserBirthdateStr);
+			} catch (ParseException e) {
+				throw new InvalidLdapAccountException(e.getMessage());
+			}
+
+			if (account.getBirthDate().compareTo(ldapUserBirthdate) != 0) {
+				logger.info("Invalid birthdate (" + ldapUserBirthdate
+						+ ") for LDAP account with supannEmpId "
+						+ account.getHarpegeNumber());
+				return false;
+			}
+
+			/*
+			 * Patronymic name checking
+			 */
+			logger.debug("Patronymic name checking");
+
+			List<String> ldapUserBirthnameList = ldapUser
+					.getAttributes(LdapSchema.getBirthName());
+			if (ldapUserBirthnameList == null
+					|| ldapUserBirthnameList.size() == 0) {
+				throw new InvalidLdapAccountException(
+						"LDAP account with supannEmpId "
+								+ account.getHarpegeNumber()
+								+ " has no patronymic name");
+			}
+
+			Iterator<String> i = ldapUserBirthnameList.iterator();
+			String sn;
+			boolean isInLdap = false;
+			while (i.hasNext()) {
+				sn = i.next();
+				if (StringTools.compareInsensitive(account.getBirthName(), sn)) {
+					isInLdap = true;
+					break;
+				}
+			}
+
+			if (!isInLdap) {
+				logger.info("Invalid patronymic ("
+						+ ldapUserBirthnameList.toArray()
+						+ ") for LDAP account with supannEmpId "
+						+ account.getHarpegeNumber());
+				return false;
+			}
+
+			account.setId(ldapUser.getId());
+			account.setDisplayName(ldapUser.getAttribute(LdapSchema
+					.getDisplayName()));
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("Setting account mail : "
+						+ ldapUser.getAttribute(LdapSchema.getMail()));
+			}
+			account.setMail(ldapUser.getAttribute(LdapSchema.getMail()));
+			
+			if (logger.isDebugEnabled()) {
+				logger.debug("Setting account shadowLastChange : "
+						+ ldapUser.getAttribute(LdapSchema.getShadowLastChange()));
+			}
+			account.setShadowLastChange(ldapUser.getAttribute(LdapSchema
+					.getShadowLastChange()));
+
+
+			account.generateInitialPassword();
+
+			/* for security reasons */
+			account.setBirthName(null);
+			account.setBirthDate(null);
+			account.setHarpegeNumber(null);
+
+			return true;
+
+		} catch (LdapException e) {
+			logger.debug("Exception thrown by validateAccount() : "
+					+ e.getMessage());
+			throw e;
+		}
+	}
+	
+	public void recupChaine(){
+		System.out.println("===============");
+		System.out.println(service.obtenirChaine());
+	}
+
+	public Information getService() {
+		return service;
+	}
+
+	public void setService(Information service) {
+		this.service = service;
+	}
+
+	
 
 }
