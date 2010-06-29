8c5f416dd2141a32925bcb44e147c58e896e6c3e
aanli <aanli@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-06-29 12:44:36 +0000

    ajout serialVersionUID dans KRBException
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@240 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-bo/src/org/esupportail/activbo/services/kerberos/KRBException.java
+++ esup-activ-bo/src/org/esupportail/activbo/services/kerberos/KRBException.java
@@ -2,6 +2,11 @@ package org.esupportail.activbo.services.kerberos;
 
 public class KRBException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1785260918948542278L;
+
 	public KRBException(String msg)
 	{
 		super(msg);
