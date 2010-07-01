54ca6b3e5367ce11ec9bc8cb87f24e0dd56a14ce
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-01 22:17:47 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@265 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
+++ esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
@@ -2,6 +2,7 @@ package org.esupportail.activfo.services.client;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import org.esupportail.commons.services.ldap.LdapException;
 
@@ -10,9 +11,9 @@ import org.esupportail.commons.services.ldap.LdapException;
 public interface AccountManagement {
 	
 	
-	public HashMap<String,String> validAccount(String number,String birthName,Date birthDate) throws LdapException;
+	public HashMap<String,String> validateAccount(String number,String birthName,Date birthDate,List<String>attrPersoInfo) throws LdapException;
 
-	public boolean updateLdap(final String currentPassword,String id,String code)throws LdapException;
+	public boolean setPassword(final String currentPassword,String id,String code)throws LdapException;
 
 	public void updateDisplayName(String displayName,String id, String code);
 	
