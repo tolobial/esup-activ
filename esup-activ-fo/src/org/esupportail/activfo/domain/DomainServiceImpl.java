3d23424c7dd7f60f2454020487b1df9f7506f36c
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-04 20:56:02 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@281 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/domain/DomainServiceImpl.java
+++ esup-activ-fo/src/org/esupportail/activfo/domain/DomainServiceImpl.java
@@ -310,17 +310,17 @@ public class DomainServiceImpl implements DomainService, InitializingBean {
 	}
 
 	
-	public void updateDisplayName(String displayName,String id, String code){
+	/*public void updateDisplayName(String displayName,String id, String code){
 		service.updateDisplayName(displayName,id,code);
 	}
 	
 	public void setMailPerso(String id,String mailPerso){
 		service.setMailPerso(id,mailPerso);
-	}
+	}*/
 	
-	public int validateCode(String id,String code){
+	/*public int validateCode(String id,String code){
 		return service.validateCode(id, code);
-	}
+	}*/
 	
 	
 
