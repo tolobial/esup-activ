d614ec3e2a8bdf99f6a87dbc2b349136809f848a
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-06 20:00:13 +0000

    BO en attente des infos personnelles venant du FO
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@318 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-bo/src/org/esupportail/activbo/domain/tools/CleaningHashCode.java
+++ esup-activ-bo/src/org/esupportail/activbo/domain/tools/CleaningHashCode.java
@@ -3,10 +3,13 @@ package org.esupportail.activbo.domain.tools;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 
 import org.esupportail.activbo.domain.beans.HashCode;
+import org.esupportail.commons.services.logging.Logger;
+import org.esupportail.commons.services.logging.LoggerImpl;
 import org.springframework.beans.factory.InitializingBean;
 
 public class CleaningHashCode extends Thread implements InitializingBean{
@@ -14,7 +17,10 @@ public class CleaningHashCode extends Thread implements InitializingBean{
 	private HashCode hashCode;
 	private String formatDateConv;
 	private String accessDateKey;
+	private String accessCodeKey;
+	private int cleaningTimeInterval;
 	
+	private final Logger logger = new LoggerImpl(getClass());
 	
 	public CleaningHashCode(){
 		//this.hashCode=hashCode;
@@ -24,24 +30,49 @@ public class CleaningHashCode extends Thread implements InitializingBean{
 	
 		try {
 			while(true){
-				System.out.println("888");
-				
-				
+				//logger.info("Boucle de nettoyage lancée");
 				if (!hashCode.isEmpty()){
-					System.out.println("111");
-					for (Map.Entry<String, HashMap<String,String>> e : hashCode.entrySet()){
-						System.out.println("222");
+					logger.info("La table de hashage n'est pas vide");
+					logger.info("=======================================================================");
+					logger.info("             TABLE DE CODES D'ACCES POUR L'APPLICATION BO            ");
+					logger.info("=======================================================================");
+					
+					Iterator<Map.Entry<String, HashMap<String,String>>> it=hashCode.entrySet().iterator();
+					while(it.hasNext()){
+						Map.Entry<String, HashMap<String,String>> e=it.next();
 						HashMap<String,String> hash=e.getValue();
+						logger.info("======= Utilisateur "+e.getKey()+"(Code --> "+hash.get("code")+"  ||  Date d'expiration --> "+hash.get(accessDateKey)+")");
+						
 						Date date=stringToDate(dateToString(new Date()));
 						
 						if (date.getTime()>this.stringToDate(hash.get(accessDateKey)).getTime()){
+							logger.info("Expiration code, Ligne utilisateur "+e.getKey()+" supprimée");
+							it.remove();
+						}
+					}
+					logger.info("=========================================================================");
+					
+					/*for (Map.Entry<String, HashMap<String,String>> e : hashCode.entrySet()){
+						HashMap<String,String> hash=e.getValue();
+						logger.info("====== Utilisateur "+e.getKey()+"============");
+						logger.info("Code --> "+hash.get("code"));
+						logger.info("Date d'expiration --> "+hash.get("date"));
+						Date date=stringToDate(dateToString(new Date()));
+						
+						if (date.getTime()>this.stringToDate(hash.get(accessDateKey)).getTime()){
+							logger.info("Delai de durée du code dépassé, Suppression du code correspondant à l'utilisateur"+e.getKey());
+=======
 							System.out.println("temps depassï¿½");
+>>>>>>> .r317
 							hashCode.remove(e.getKey());
+							//System.out.println(e.getValue().toString());
 						}  
 					} 
+					logger.info("===================================");*/
+					
 				}
 				//mettre le temps dans un fichier de properties
-				sleep(10);
+				sleep(cleaningTimeInterval);
 				
 			}
 		
@@ -98,6 +129,22 @@ public class CleaningHashCode extends Thread implements InitializingBean{
 
 	public void setHashCode(HashCode hashCode) {
 		this.hashCode = hashCode;
+	}
+
+	public int getCleaningTimeInterval() {
+		return cleaningTimeInterval;
+	}
+
+	public void setCleaningTimeInterval(int cleaningTimeInterval) {
+		this.cleaningTimeInterval = cleaningTimeInterval;
+	}
+
+	public String getAccessCodeKey() {
+		return accessCodeKey;
+	}
+
+	public void setAccessCodeKey(String accessCodeKey) {
+		this.accessCodeKey = accessCodeKey;
 	}
 	
 }
