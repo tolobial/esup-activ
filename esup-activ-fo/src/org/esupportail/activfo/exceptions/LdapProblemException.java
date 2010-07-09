294c95b6f7e446e9d8b38813d790f07548a0180c
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-09 06:15:14 +0000

    Modification information personnelles possible
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@327 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/exceptions/LdapProblemException.java
@@ -0,0 +1,33 @@
+package org.esupportail.activfo.exceptions;
+
+public class LdapProblemException extends Exception{
+	
+	/**
+	 * The id for serialization.
+	 */
+	private static final long serialVersionUID = 8197090501242229324L;
+
+	/**
+	 * @param message
+	 */
+	public LdapProblemException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public LdapProblemException(final Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public LdapProblemException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+
+}
