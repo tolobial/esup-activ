e0094bc04b5e773d4135af56dad7f2861a36fdf0
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-09 06:18:18 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@334 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-bo/src/org/esupportail/activbo/exceptions/UserPermissionException.java
@@ -0,0 +1,33 @@
+package org.esupportail.activbo.exceptions;
+
+public class UserPermissionException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8375439753015128832L;
+
+	/**
+	 * @param message
+	 */
+	public UserPermissionException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public UserPermissionException(final Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public UserPermissionException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+
+}
