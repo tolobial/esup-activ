35cdb80149dea5a83ac3d5f62299e9fdbb8883eb
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-09 06:14:54 +0000

    Modification information personnelles possible
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@326 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
+++ esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.esupportail.activfo.exceptions.LdapProblemException;
+import org.esupportail.activfo.exceptions.UserPermissionException;
 import org.esupportail.commons.services.ldap.LdapException;
 
 
@@ -11,11 +13,11 @@ import org.esupportail.commons.services.ldap.LdapException;
 public interface AccountManagement {
 	
 	
-	public HashMap<String,String> validateAccount(String number,String birthName,Date birthDate,List<String>attrPersoInfo) throws LdapException;
+	public HashMap<String,String> validateAccount(String number,String birthName,Date birthDate,List<String>attrPersoInfo) throws LdapProblemException;
 
-	public boolean setPassword(String id,String code,final String currentPassword)throws LdapException;
+	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException;
 
-	public boolean updateInfoPerso(HashMap<String,String> infoPerso);
+	public boolean updateInfoPerso(String id,String code,HashMap<String,String> infoPerso)throws LdapProblemException,UserPermissionException;
 	
 	//public void updateDisplayName(String displayName,String id, String code);
 	
