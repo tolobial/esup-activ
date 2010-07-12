71367f682a0e5f5fbb2c42168f6395c64b21185a
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-12 13:12:45 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@341 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
+++ esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.esupportail.activfo.exceptions.KerberosException;
 import org.esupportail.activfo.exceptions.LdapProblemException;
 import org.esupportail.activfo.exceptions.UserPermissionException;
 import org.esupportail.commons.services.ldap.LdapException;
@@ -15,7 +16,7 @@ public interface AccountManagement {
 	
 	public HashMap<String,String> validateAccount(String number,String birthName,Date birthDate,List<String>attrPersoInfo) throws LdapProblemException;
 
-	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException;
+	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException;
 
 	public boolean updateInfoPerso(String id,String code,HashMap<String,String> infoPerso)throws LdapProblemException,UserPermissionException;
 	
