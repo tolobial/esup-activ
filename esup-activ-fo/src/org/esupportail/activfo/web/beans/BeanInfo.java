5db7e00b2a2e9a4ecc1560266bf1137f1f2cfbfb
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-14 12:50:42 +0000

    Activation en attente de test
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@355 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/beans/BeanInfo.java
@@ -0,0 +1,31 @@
+package org.esupportail.activfo.web.beans;
+
+import org.esupportail.activfo.web.validators.ValidatorDisplayName;
+
+public interface BeanInfo {
+	
+	public String getKey();
+
+
+	public void setKey(String key);
+
+
+	public String getValue();
+
+
+	public void setValue(String value);
+
+
+	
+	public String getTypeChamp();
+
+
+	public void setTypeChamp(String typeChamp);
+
+
+	public String getAide();
+
+
+	public void setAide(String aide);
+
+}
