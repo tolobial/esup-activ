48263bf81c3189101f52aacd0509234dd3650551
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-14 12:47:33 +0000

    Activation en attente de test
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@347 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-bo/src/org/esupportail/activbo/domain/DomainService.java
+++ esup-activ-bo/src/org/esupportail/activbo/domain/DomainService.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 
 import org.esupportail.activbo.domain.beans.User;
+import org.esupportail.activbo.exceptions.KerberosException;
 import org.esupportail.activbo.exceptions.LdapProblemException;
 import org.esupportail.activbo.exceptions.UserPermissionException;
 import org.esupportail.commons.exceptions.ConfigException;
@@ -122,7 +123,7 @@ public interface DomainService extends Serializable {
 		
 	public HashMap<String,String> validateAccount(String number,String birthName,Date birthDate,List<String>attrPersoInfo) throws LdapProblemException;
 	
-	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException;
+	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException;
 	
 	public boolean updateInfoPerso(String id,String code,HashMap<String,String> infoPerso) throws LdapProblemException,UserPermissionException;
 	
