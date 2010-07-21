c628010f53bba8b1b99d6224cc885ad56ddbc6e9
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-21 17:12:07 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@360 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/validators/Validator.java
@@ -0,0 +1,16 @@
+package org.esupportail.activfo.web.validators;
+
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import org.esupportail.commons.beans.AbstractI18nAwareBean;
+
+
+public interface Validator  {
+	
+
+	public void validate(FacesContext context, UIComponent componentToValidate,Object value) throws ValidatorException;
+		
+}
