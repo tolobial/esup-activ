ef8f32bf0d634db346af2c117161b0c696c058f3
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-07-27 22:02:23 +0000

    FO, aide mise en place
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@382 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/beans/BeanFieldImpl.java
@@ -0,0 +1,61 @@
+package org.esupportail.activfo.web.beans;
+
+import org.esupportail.activfo.web.validators.Validator;
+import org.esupportail.activfo.web.validators.ValidatorDisplayName;
+
+public class BeanFieldImpl implements BeanField {
+	
+	private String key;
+	private String value;
+	private String typeChamp;
+	private String aide;
+	private String converter;
+	private Validator validator;
+	private String id;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getTypeChamp() {
+		return typeChamp;
+	}
+	public void setTypeChamp(String typeChamp) {
+		this.typeChamp = typeChamp;
+	}
+	public String getAide() {
+		return aide;
+	}
+	public void setAide(String aide) {
+		this.aide = aide;
+	}
+	public Validator getValidator() {
+		return validator;
+	}
+	public void setValidator(Validator validator) {
+		this.validator = validator;
+	}
+	public String getConverter() {
+		return converter;
+	}
+	public void setConverter(String converter) {
+		this.converter = converter;
+	}
+	
+}
