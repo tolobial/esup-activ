190559b0882c68f48d1f8695468b5158f26ae667
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-08-13 22:03:58 +0000

    Corrections FO apres entretien 12 aout
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@475 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/validators/ValidatorStudentLogin.java
@@ -0,0 +1,36 @@
+package org.esupportail.activfo.web.validators;
+
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+
+import org.esupportail.commons.beans.AbstractI18nAwareBean;
+
+
+public class ValidatorStudentLogin extends AbstractI18nAwareBean implements Validator{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8849185735359561457L;
+
+	/**
+	 * 
+	 */
+	
+	
+	
+
+	public void validate(FacesContext context, UIComponent componentToValidate,Object value) throws ValidatorException {
+		if (value instanceof String) {
+			String strValue = (String) value;
+			
+			if (!strValue.matches("^[0-9]{11}$")) {
+				throw new ValidatorException(getFacesErrorMessage("VALIDATOR.STUDENTLOGIN.INVALID"));
+			}
+		}
+		else throw new ValidatorException(getFacesErrorMessage("VALIDATOR.NOTSTRING"));
+	}
+	
+}
