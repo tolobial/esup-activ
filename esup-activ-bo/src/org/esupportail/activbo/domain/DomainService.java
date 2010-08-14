31fe919ad3ef68257f3778f7d45d0bd6f7c539e0
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-08-14 11:41:12 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@478 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-bo/src/org/esupportail/activbo/domain/DomainService.java
+++ esup-activ-bo/src/org/esupportail/activbo/domain/DomainService.java
@@ -124,15 +124,15 @@ public interface DomainService extends Serializable {
 		
 	public HashMap<String,String> validateAccount(HashMap<String,String> hashInfToValidate,List<String>attrPersoInfo) throws LdapProblemException;
 	
-	public boolean setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException;
+	public void setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException;
 	
-	public boolean updatePersonalInformations(String id,String code,HashMap<String,String> hashBeanPersoInfo) throws LdapProblemException,UserPermissionException;
+	public void updatePersonalInformations(String id,String code,HashMap<String,String> hashBeanPersoInfo) throws LdapProblemException,UserPermissionException;
 	
 	public boolean getCode(String id,String canal)throws LdapProblemException;
 	
 	public boolean validateCode(String id,String code);
 	
-	public boolean changeLogin(String id, String code,String newLogin)throws LdapProblemException,UserPermissionException,KerberosException,LoginAlreadyExistsException;
+	public void changeLogin(String id, String code,String newLogin)throws LdapProblemException,UserPermissionException,KerberosException,LoginAlreadyExistsException;
 	
 	public HashMap<String,String> authentificateUser(String id,String password,List<String>attrPersoInfo)throws AuthentificationException,LdapProblemException;
 	
