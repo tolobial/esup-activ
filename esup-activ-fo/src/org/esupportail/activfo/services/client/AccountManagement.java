4232a520b0e30c0188a7188e3c1c4e1c36b419ae
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-08-27 15:19:41 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@522 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
+++ esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.esupportail.activfo.exceptions.AuthentificationException;
-import org.esupportail.activfo.exceptions.LoginAlreadyExistsException;
 import org.esupportail.activfo.exceptions.KerberosException;
 import org.esupportail.activfo.exceptions.LdapProblemException;
+import org.esupportail.activfo.exceptions.LoginAlreadyExistsException;
 import org.esupportail.activfo.exceptions.UserPermissionException;
 
 
