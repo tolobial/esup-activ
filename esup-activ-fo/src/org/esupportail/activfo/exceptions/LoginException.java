87024c0e6078590d66a8149ad1f5927b3e4e9ec3
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-08-31 11:00:59 +0000

    Ajout LoginExcpeiton
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@556 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/exceptions/LoginException.java
@@ -0,0 +1,33 @@
+package org.esupportail.activfo.exceptions;
+
+public class LoginException extends Exception{
+	
+	/**
+	 * The id for serialization.
+	 */
+	private static final long serialVersionUID = 8197090501242229324L;
+
+	/**
+	 * @param message
+	 */
+	public LoginException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public LoginException(final Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public LoginException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+
+}
