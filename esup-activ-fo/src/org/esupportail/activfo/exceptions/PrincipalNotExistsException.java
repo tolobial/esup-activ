6305a38375bce059c30d398187617bf17fc9a636
lorvivien <lorvivien@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-09-02 16:09:09 +0000

    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@608 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/exceptions/PrincipalNotExistsException.java
@@ -0,0 +1,33 @@
+package org.esupportail.activfo.exceptions;
+
+public class PrincipalNotExistsException extends Exception{
+	
+	/**
+	 * The id for serialization.
+	 */
+	private static final long serialVersionUID = 8197090501242229324L;
+
+	/**
+	 * @param message
+	 */
+	public PrincipalNotExistsException(final String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public PrincipalNotExistsException(final Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public PrincipalNotExistsException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+
+}
