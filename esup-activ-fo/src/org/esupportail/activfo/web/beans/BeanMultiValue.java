5bf8c95bee143c142104f51f6dbe89f90531d2d0
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-12-10 10:59:08 +0000

    Beans pour les attributs multivalués
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@677 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/beans/BeanMultiValue.java
@@ -0,0 +1,19 @@
+package org.esupportail.activfo.web.beans;
+
+/**
+ * @author BANG
+ *
+ */
+public interface BeanMultiValue {
+	
+	/**
+	 * @return
+	 */
+	public String getValue();
+	
+	/**
+	 * @param value
+	 */
+	public void setValue(String value);
+
+}
---
+++ esup-activ-fo/src/org/esupportail/activfo/web/beans/BeanMultiValueImpl.java
@@ -0,0 +1,31 @@
+package org.esupportail.activfo.web.beans;
+
+/**
+ * @author BANG
+ *
+ */
+public class BeanMultiValueImpl implements BeanMultiValue{
+  /**
+ * 
+ */
+private String value;
+
+/**
+ * @return the value
+ */
+public String getValue() {
+	return value;
+}
+
+/**
+ * @param value the value to set
+ */
+public void setValue(String value) {
+	this.value = value;
+}
+
+
+
+  
+ 	
+}
