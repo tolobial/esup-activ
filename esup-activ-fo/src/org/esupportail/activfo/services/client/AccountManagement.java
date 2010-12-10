ebf9a6000a91fc5f27e264fea417a5bcdb2cbf38
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2010-12-10 12:08:33 +0000

    Modification des méthodes (validateAccount et updatePersonalInformations)
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@681 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
+++ esup-activ-fo/src/org/esupportail/activfo/services/client/AccountManagement.java
@@ -1,11 +1,13 @@
 package org.esupportail.activfo.services.client;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 
-import org.esupportail.activfo.exceptions.AuthentificationException;
 import org.esupportail.activfo.exceptions.ChannelException;
+import org.esupportail.activfo.exceptions.AuthentificationException;
 import org.esupportail.activfo.exceptions.KerberosException;
 import org.esupportail.activfo.exceptions.LdapProblemException;
 import org.esupportail.activfo.exceptions.LoginAlreadyExistsException;
@@ -14,26 +16,21 @@ import org.esupportail.activfo.exceptions.PrincipalNotExistsException;
 import org.esupportail.activfo.exceptions.UserPermissionException;
 
 
-
 public interface AccountManagement {
 	
-	
-	public HashMap<String,String> validateAccount(HashMap<String,String> hashInfToValidate,List<String>attrPersoInfo) throws LdapProblemException,AuthentificationException,LoginException;
+	public HashMap<String, String> validateAccount(HashMap<String,String> hashInfToValidate,List<String>attrPersoInfo) throws LdapProblemException,AuthentificationException, LoginException;
 
-	public void setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException,LoginException;
+	public void setPassword(String id,String code,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException, LoginException;
 	
-	public void setPassword(String id,String code,String newLogin,final String currentPassword)throws LdapProblemException,UserPermissionException,KerberosException,LoginException;
-
-	public void updatePersonalInformations(String id,String code,HashMap<String,String> hashBeanPersoInfo)throws LdapProblemException,UserPermissionException,LoginException;
-	
-	public boolean validateCode(String id,String code)throws UserPermissionException;
+	public void setPassword(String id,String code,String newLogin, final String currentPassword) throws LdapProblemException,UserPermissionException,KerberosException, LoginException;
+		
+	public void updatePersonalInformations(String id,String code,HashMap<String,String> hashBeanPersoInfo)throws LdapProblemException,UserPermissionException, LoginException;
 	
 	public void sendCode(String id,String canal)throws ChannelException;
 		
-	public HashMap<String,String> authentificateUser(String id,String password,List<String>attrPersoInfo)throws AuthentificationException,LdapProblemException,UserPermissionException,LoginException;
-
-	public void changeLogin(String id,String code,String newLogin)throws LdapProblemException,UserPermissionException,KerberosException,LoginAlreadyExistsException,LoginException,PrincipalNotExistsException;
+	public boolean validateCode(String id,String code)throws UserPermissionException;
 	
+	public void changeLogin(String id, String code,String newLogin)throws LdapProblemException,UserPermissionException,KerberosException,LoginAlreadyExistsException, LoginException,PrincipalNotExistsException;
 	
-	
+	public HashMap<String,String> authentificateUser(String id,String password,List<String>attrPersoInfo)throws AuthentificationException,LdapProblemException,UserPermissionException, LoginException;
 }
