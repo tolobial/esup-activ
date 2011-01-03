d9f1d05760604528316f92970ee8d211b1d52743
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-01-03 10:10:13 +0000

    Function jqeury with hide and show : If clicked on hide, value will be deleted
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@708 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/webapp/media/form-show-hide.js
+++ esup-activ-fo/webapp/media/form-show-hide.js
@@ -4,21 +4,15 @@ $(function() {
 	$(".show").click(function () {
 	    $("." + $(this).attr("alt")+"hide:first").show();
 	    $("." + $(this).attr("alt")+"hide:first").removeClass().addClass($(this).attr("alt")+"show");
-	  
-	    });
-	$(".hide").click(function () {
-	    
-		//if ( $("." + $(this).attr("alt")+"show").children("#old")   ) {
-		if( $("." + $(this).attr("alt")+"show:last").children("#old")         ) {
-		   $("." + $(this).attr("alt")+"show:last").hide();
-		   $("." + $(this).attr("alt")+"show:last").removeClass().addClass($(this).attr("alt")+"hide");
-	     }
-
-	      
-	     
-	    });
+	});
 	
-
+	$(".hide").click(function () {
+		if( $("." + $(this).attr("alt")+"show").size() > 1    ) {
+			$("." + $(this).attr("alt")+"show:last").hide();
+		    $("." + $(this).attr("alt")+"show:last").children("input:text").val("");
+		    $("." + $(this).attr("alt")+"show:last").removeClass().addClass($(this).attr("alt")+"hide");
+		}
+	});
 });
 
 
