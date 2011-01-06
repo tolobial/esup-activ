761b7793a7664c852732d4831a388efc000087a0
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-01-06 15:04:56 +0000

    Proposition of newlogin with converter
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@732 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/converters/NewLoginConverter.java
@@ -0,0 +1,80 @@
+/**
+ * ESUP-Portail Commons - Copyright (c) 2006-2009 ESUP-Portail consortium.
+ */
+package org.esupportail.activfo.web.converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import org.esupportail.activfo.domain.beans.Account;
+
+/**
+ * A JSF converter to pass Integer instances.
+ */
+public class NewLoginConverter implements Converter {
+	
+	private Account account;
+	private String displayName;
+	
+    
+
+	public NewLoginConverter() {
+    }
+ 
+    public Object getAsObject(
+    		@SuppressWarnings("unused") final FacesContext context, 
+			@SuppressWarnings("unused") final UIComponent component, 
+			final String value){
+        return value;
+    }
+ 
+    public String getAsString(
+    		@SuppressWarnings("unused") final FacesContext context, 
+			@SuppressWarnings("unused") final UIComponent component, 
+			final Object value) {
+    	
+    	String stringTrim[] = null;
+    	String newLogin="";
+    	String var = account.getAttribute(this.displayName);
+    	stringTrim=var.split(" ");
+    	for(int i=0;i<stringTrim.length;i++) {
+    		if (i!=stringTrim.length-1)
+    			newLogin+=stringTrim[i].substring(0,1);
+    		else newLogin+=stringTrim[i];
+    	}
+      return newLogin.trim().toLowerCase();
+    }
+
+	/**
+	 * @return the account
+	 */
+	public Account getAccount() {
+		return account;
+	}
+
+	/**
+	 * @param account the account to set
+	 */
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+
+	/**
+	 * @return the displayName
+	 */
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	/**
+	 * @param displayName the displayName to set
+	 */
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+    
+   
+	
+}
