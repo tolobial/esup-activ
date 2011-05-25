631f73ee97420fd117aa5029c9d0dff163003125
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-05-25 12:29:19 +0000

    Nouveau convertiseur MapOrDefaultConverter pour eduPersonAffiliation
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@968 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/src/org/esupportail/activfo/web/converters/MapOrDefaultConverter.java
@@ -0,0 +1,70 @@
+/**
+ * ESUP-Portail Commons - Copyright (c) 2006-2009 ESUP-Portail consortium.
+ */
+package org.esupportail.activfo.web.converters;
+
+import java.util.HashMap;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import org.esupportail.commons.services.logging.Logger;
+import org.esupportail.commons.services.logging.LoggerImpl;
+
+/**
+ * A JSF converter to pass Integer instances.
+ */
+public class MapOrDefaultConverter implements Converter {
+	
+	private HashMap<String,String> mapping = new HashMap<String,String>();
+	
+	private String returnValue="";
+	
+	private final Logger logger = new LoggerImpl(getClass());
+
+	public MapOrDefaultConverter() {
+    }
+ 
+    public Object getAsObject(
+    		@SuppressWarnings("unused") final FacesContext context, 
+			@SuppressWarnings("unused") final UIComponent component, 
+			final String value){
+        return value;
+    }
+ 
+    public String getAsString(
+    		@SuppressWarnings("unused") final FacesContext context, 
+			@SuppressWarnings("unused") final UIComponent component, 
+			final Object value) {
+     	
+    	logger.debug("value.toString : "+value.toString());
+    	
+    	if (mapping.get(value)==null) {
+    		//logger.debug("Isn't blank ? "+value);
+    		return getReturnValue();
+    	}
+    	else return mapping.get(value).toString();
+    	
+    }
+
+	/**
+	 * @return the mapping
+	 */
+	public HashMap<String, String> getMapping() {
+		return mapping;
+	}
+
+	/**
+	 * @param mapping the mapping to set
+	 */
+	public void setMapping(HashMap<String, String> mapping) {
+		this.mapping = mapping;
+	}
+	
+	public String getReturnValue() {
+		return returnValue;
+	}
+
+		
+}
