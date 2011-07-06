025f6ecc3a532f32c73c1fac7d4b33366e2d87d6
aanli <aanli@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-07-06 09:58:18 +0000

    FO: Prise en compte automatique du changement de version
    Lorsqu'on passait à une autre version (supérieure), il y avait un message d'erreur "VersionException Message The database is too old (2.1.0), please run 'ant upgrade'..."
    
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1005 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/dao/EmptyDaoService.java
+++ esup-activ-fo/src/org/esupportail/activfo/dao/EmptyDaoService.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 import org.esupportail.activfo.domain.beans.User;
 import org.esupportail.activfo.domain.beans.VersionManager;
+import org.esupportail.commons.utils.Assert;
 import org.esupportail.commons.web.beans.Paginator;
+import org.esupportail.commons.services.application.ApplicationService;
+import org.springframework.beans.factory.InitializingBean;
 
-public class EmptyDaoService implements DaoService {
+public class EmptyDaoService implements DaoService, InitializingBean {
 
 	private static final long serialVersionUID = 3429875756876395115L;
 	
 	private static VersionManager versionManager;
-
+    private ApplicationService applicationService;
 	
+	/* (non-Javadoc)
+	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(this.applicationService, 
+				"property applicationService of class " + this.getClass().getName() + " can not be null");			
+	}
 	
 	public void addUser(User user) {
 		// TODO Auto-generated method stub
@@ -65,9 +75,22 @@ public class EmptyDaoService implements DaoService {
 		// TODO Auto-generated method stub
 		if(versionManager==null)		{
 			versionManager = new VersionManager();
-			versionManager.setVersion("2.1.1");
+			versionManager.setVersion(applicationService.getVersion().toString());
 		}
 		return versionManager;		
 	}
 
+	/**
+	 * @return the applicationService
+	 */
+	public ApplicationService getApplicationService() {
+		return applicationService;
+	}
+
+	/**
+	 * @param applicationService the applicationService to set
+	 */
+	public void setApplicationService(ApplicationService applicationService) {
+		this.applicationService = applicationService;
+	}
 }
