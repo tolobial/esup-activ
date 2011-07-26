b176effcab942ea8f4f0af56853773fa9d35eed0
bang <bang@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-07-26 09:19:53 +0000

    changement algorithme validatorDisplayName
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1034 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/web/validators/ValidatorDisplayName.java
+++ esup-activ-fo/src/org/esupportail/activfo/web/validators/ValidatorDisplayName.java
@@ -1,6 +1,9 @@
 package org.esupportail.activfo.web.validators;
 
 import java.text.Collator;
+import java.text.Normalizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -21,6 +24,7 @@ public class ValidatorDisplayName extends AbstractI18nAwareBean implements Valid
 
 	private Account account;
 	private String displayNameAttr;
+	private String caracterForbiddenDisplayName;
 	
 	/**
 	 * 
@@ -29,21 +33,46 @@ public class ValidatorDisplayName extends AbstractI18nAwareBean implements Valid
 	public void validate(FacesContext context, UIComponent componentToValidate,Object value) throws ValidatorException {
 		String val=(String)value;
 		
+		Pattern p;
+		Matcher m;
+		String specialmessage=null;
+		
+		p=Pattern.compile(caracterForbiddenDisplayName);
+		m=p.matcher(val);
+			
+		if(! m.find() || m.group(0).equals(" ")) {
+			if (this.isContainFirstName(val) && this.isContainLastName(val)) {
+				
+			} else {
+				throw new ValidatorException(getFacesErrorMessage("VALIDATOR.DISPLAYNAME.INVALID"));
+			}
+			
+		} else {
+			specialmessage=m.group(0);
+			throw new ValidatorException(getFacesErrorMessage("VALIDATOR.PASSWORD.CARACTERFORBIDDEN",specialmessage));
+		}
+		
+		/*
 		if (this.compareInsensitive(account.getAttribute(this.displayNameAttr), val)) {
 			
 		}
 		else{
 			throw new ValidatorException(getFacesErrorMessage("VALIDATOR.DISPLAYNAME.INVALID"));
 		}
+		*/
 	}
 	
 	public boolean compareInsensitive(String str1, String str2) {
 
 		if (str2==null) return false;
+		
+		logger.debug("str1 et str2 sont "+str1+", "+str2);
+		
 		String strTmp1 = str1.replaceAll("[-|']+", " ");
 		String strTmp2 = str2.replaceAll("[-|']+", " ");
 		Collator collator = Collator.getInstance();
 		collator.setStrength(Collator.PRIMARY);
+		
 
 		if (collator.compare(strTmp1, strTmp2) == 0) {
 			logger.debug("Strings are equivalent");
@@ -53,6 +82,64 @@ public class ValidatorDisplayName extends AbstractI18nAwareBean implements Valid
 		logger.debug("Strings  are different");
 		return false;
 	}
+	
+	private boolean isContainFirstName(String firstName)
+	{
+		boolean permit=false;
+		
+		String compareSN=account.getAttribute("sn").toLowerCase();
+		String compareUp1BirthName=account.getAttribute("up1BirthName").toLowerCase();
+				
+    	/*
+    	
+    	String stringTrim[] = null;
+		stringTrim=returnValue.toLowerCase().split(" ");
+		
+    	for(int i=0;i<stringTrim.length;i++) {
+    		if (compareSN.contains(stringTrim[i]) || compareUp1BirthName.contains(stringTrim[i]))
+    			permit=true;
+    	}
+    	*/
+    	if (firstName.toLowerCase().contains(compareSN)) {
+    		logger.debug("True : The string("+firstName+") contains SN ("+compareSN+")");
+    		permit=true;
+    	}
+    	if (firstName.toLowerCase().contains(compareUp1BirthName)) {
+    		logger.debug("True : The string("+firstName+") contains up1BirthName ("+compareUp1BirthName+")");
+    		permit=true;
+    	}
+    	
+    	return permit;
+	}
+
+	private boolean isContainLastName(String lastName)
+	{
+		boolean permit=false;
+		
+		String compareGivenName=account.getAttribute("givenName").toLowerCase();
+		String compareUp1AltGivenName=account.getAttribute("up1AltGivenName").toLowerCase();
+				
+    	/*
+    	
+    	String stringTrim[] = null;
+		stringTrim=returnValue.toLowerCase().split(" ");
+		
+    	for(int i=0;i<stringTrim.length;i++) {
+    		if (compareGivenName.contains(stringTrim[i]) || compareUp1AltGivenName.contains(stringTrim[i]))
+    			permit=true;
+    	}
+    	*/
+    	if (lastName.toLowerCase().contains(compareGivenName)) {
+    		logger.debug("True : The string("+lastName+") contains givenName("+compareGivenName+")");
+    		permit=true;
+    	}
+    	if (lastName.toLowerCase().contains(compareUp1AltGivenName)) {
+    		logger.debug("True : The string("+lastName+") contains up1AltGivenName("+compareUp1AltGivenName+")");
+    		permit=true;
+    	}
+    	return permit;
+	}
+	
 	
 	public static String cleanAllSpecialChar(String str) {
 
@@ -86,5 +173,21 @@ public class ValidatorDisplayName extends AbstractI18nAwareBean implements Valid
 	public void setDisplayNameAttr(String displayNameAttr) {
 		this.displayNameAttr = displayNameAttr;
 	}
+
+	/**
+	 * @return the caracterForbiddenDisplayName
+	 */
+	public String getCaracterForbiddenDisplayName() {
+		return caracterForbiddenDisplayName;
+	}
+
+	/**
+	 * @param caracterForbiddenDisplayName the caracterForbiddenDisplayName to set
+	 */
+	public void setCaracterForbiddenDisplayName(String caracterForbiddenDisplayName) {
+		this.caracterForbiddenDisplayName = caracterForbiddenDisplayName;
+	}
+	
+	
 	
 }
