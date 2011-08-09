d474442daf80550c07d1db3fd0548f2ac1797103
aanli <aanli@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-08-09 21:07:21 +0000

    Afficher un champ lorsqu'un champ multi-valué ne contient aucune valeur
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1052 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/webapp/media/form-show-hide.js
+++ esup-activ-fo/webapp/media/form-show-hide.js
@@ -1,6 +1,12 @@
 
 $(function() {
-
+    $(".hide").load(function() {
+    	if( $("." + $(this).attr("alt")+"show").size()==0){
+    		$("." + $(this).attr("alt")+"hide:first").show();
+    		$("." + $(this).attr("alt")+"hide:first").removeClass().addClass($(this).attr("alt")+"show");
+    	}
+    });
+	
 	$(".show").click(function () {
 	    $("." + $(this).attr("alt")+"hide:first").show();
 	    $("." + $(this).attr("alt")+"hide:first").removeClass().addClass($(this).attr("alt")+"show");
