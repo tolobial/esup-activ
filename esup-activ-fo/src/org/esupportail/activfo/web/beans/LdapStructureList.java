8bdc00e16674366ddf35f7ebdc4f9f9fc64bc1fa
aanli <aanli@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-08-16 10:01:54 +0000

    Corrections code chargemenent dynamique de la liste déroulante des structures à partir de LDAP
    - les structures sont chargées suivant des catégories paramétrées
    - structures regroupées par catégorie
    - les structures sont ordonnées dans chaque catégorie
    - chargement des structures à partir de LDAP en singleton (au lieu de session)
    
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1058 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-fo/src/org/esupportail/activfo/web/beans/LdapStructureList.java
+++ esup-activ-fo/src/org/esupportail/activfo/web/beans/LdapStructureList.java
@@ -1,7 +1,11 @@
 package org.esupportail.activfo.web.beans;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import org.esupportail.commons.services.ldap.LdapEntity;
 import org.esupportail.commons.services.ldap.LdapEntityService;
@@ -11,26 +15,57 @@ import org.esupportail.commons.services.logging.LoggerImpl;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 
 public class LdapStructureList extends ArrayList<SelectItem> implements InitializingBean {
 	
+	/**
+	 * Permet de charger dynamiquement la liste des structures à partir de LDAP
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private final Logger logger = new LoggerImpl(getClass());
 	
 	private LdapEntityService ldapEntityService;
-	
-	private String ldapSearchFilter;
+	private String ldapSearchFilter="(objectClass=supannEntite)";
+	private String businessCategory="businessCategory";
+	private String firstLabel=""; 
+	private String label="description";	
+	private String value="supannCodeEntite";
+	private HashMap<String,String> businessCategoryMap;
 	
 	public void afterPropertiesSet() throws Exception {
+		SelectItem si = new SelectItem();		
+		si.setLabel(firstLabel);	
+		si.setValue("");
+		this.add(si);
 		
-		List<LdapEntity> ldapEntities = ldapEntityService.getLdapEntitiesFromFilter(ldapSearchFilter);
-		logger.debug("filter is "+ldapSearchFilter);
-		
-		for(int i=0;i<ldapEntities.size();i++)
-		{
-			SelectItem si = new SelectItem();
-			si.setLabel(ldapEntities.get(i).getAttribute("description"));
-			si.setValue(ldapEntities.get(i).getAttribute("supannCodeEntite"));
-			this.add(si);
+		Set<String> keys= businessCategoryMap.keySet();
+		for(String key:keys){
+			SelectItemGroup sig =new SelectItemGroup();
+			sig.setLabel(businessCategoryMap.get(key));
+									
+			String filter="(&("+businessCategory+"="+key+")"+ldapSearchFilter+")";
+			logger.debug("Bussiness category Filter is : "+ filter);
+			List<LdapEntity> ldapEntities = ldapEntityService.getLdapEntitiesFromFilter(filter);
+			SelectItem[] entities=new SelectItem[ldapEntities.size()];
+			int i=0;
+			for(LdapEntity ldapEntity:ldapEntities)
+			{
+				si = new SelectItem();
+				si.setLabel(ldapEntity.getAttribute(label));
+				si.setValue(ldapEntity.getAttribute(value));
+				entities[i++]=si;
+			}
+			
+			Arrays.sort(entities,new Comparator<SelectItem>(){
+			    public int compare(SelectItem o1, SelectItem o2) {
+			         return o1.getLabel().compareTo(o2.getLabel());
+			    }
+			});		
+			
+			sig.setSelectItems(entities);
+			this.add(sig);
 		}
 	}
 	
@@ -61,6 +96,69 @@ public class LdapStructureList extends ArrayList<SelectItem> implements Initiali
 	 */
 	public void setLdapSearchFilter(String ldapSearchFilter) {
 		this.ldapSearchFilter = ldapSearchFilter;
+	}
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * @return the firstLabel
+	 */
+	public String getFirstLabel() {
+		return firstLabel;
+	}
+
+	/**
+	 * @param firstLabel the firstLabel to set
+	 */
+	public void setFirstLabel(String firstLabel) {
+		this.firstLabel = firstLabel;
+	}
+
+	/**
+	 * @param businessCategory the businessCategory attribute to use
+	 */
+	public void setBusinessCategory(String businessCategory) {
+		this.businessCategory = businessCategory;
+	}
+
+	/**
+	 * @return the businessCategoryMap
+	 */
+	public HashMap<String, String> getBusinessCategoryMap() {
+		return businessCategoryMap;
+	}
+
+	/**
+	 * @param businessCategoryMap the businessCategoryMap to set
+	 */
+	public void setBusinessCategoryMap(HashMap<String, String> businessCategoryMap) {
+		this.businessCategoryMap = businessCategoryMap;
 	}
 		
 }
