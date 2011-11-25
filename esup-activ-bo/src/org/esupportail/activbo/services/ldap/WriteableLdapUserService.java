6b8d90bb85c1ab49cc555f44b447038643f22004
aanli <aanli@df3935f2-5e34-0410-be39-dc869cc82e10>
2011-11-25 15:17:27 +0000

    Code cleanup: WriteableLdapUserSerice.java: réutilisation des codes de WriteableLdapUserSerice de esup-commons
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1148 df3935f2-5e34-0410-be39-dc869cc82e10

--- esup-activ-bo/src/org/esupportail/activbo/services/ldap/WriteableLdapUserService.java
+++ esup-activ-bo/src/org/esupportail/activbo/services/ldap/WriteableLdapUserService.java
@@ -3,55 +3,17 @@
  */
 package org.esupportail.activbo.services.ldap;
 
-import java.io.Serializable;
-
 import org.esupportail.activbo.exceptions.AuthentificationException;
-import org.esupportail.commons.services.ldap.LdapException;
 import org.esupportail.commons.services.ldap.LdapUser;
 
 /**
  * The interface of writeable LDAP user services.
  */
-public interface WriteableLdapUserService extends Serializable {
-
-	/** 
-	 * Update a LDAP user in the LDAP directory.
-	 * @param ldapUser
-	 */
-	void updateLdapUser(LdapUser ldapUser);
+public interface WriteableLdapUserService extends org.esupportail.commons.services.ldap.WriteableLdapUserService {
+	public void defineAuthenticatedContext(String username, String password);
+	public void defineAuthenticatedContextForUser(String userId, String password);
 	
-	/** 
-	 * Create a LDAP user.
-	 * @param ldapUser
-	 */
-	void createLdapUser(LdapUser ldapUser);
-	
-	/** 
-	 * Delete a LDAP user.
-	 * @param ldapUser
-	 */
-	void deleteLdapUser(LdapUser ldapUser);
-	
-	/** 
-	 * Define credentials which will be used for LDAP binding.
-	 * Usefull when you ask credentials via a forms in your application
-	 * and you want to bind to LDAP with those credentials.
-	 * @param userId
-	 * @param password
-	 */
-	void setAuthenticatedContext(String userId, String password);
-	
-	/** 
-	 * Disable WriteableLdapUserService write capabilities
-	 * so that it can't write in LDAP directory in the sequel.
-	 */
-	void defineAnonymousContext();
-	
-	public void defineAuthenticatedContext(String userId, String password) throws LdapException;
-	
-	public void defineAuthenticatedContextForUser(String userId, String password) throws LdapException;
-	
-	public void bindLdap(final LdapUser ldapUser)throws AuthentificationException;
+	public void bindLdap(final LdapUser ldapUser) throws AuthentificationException;
 	
 	public void invalidateLdapCache();
 }
--- esup-activ-bo/src/org/esupportail/activbo/services/ldap/WriteableLdapUserServiceImpl.java
+++ esup-activ-bo/src/org/esupportail/activbo/services/ldap/WriteableLdapUserServiceImpl.java
@@ -14,67 +14,27 @@ import org.esupportail.commons.services.ldap.LdapException;
 import org.esupportail.commons.services.ldap.LdapUser;
 import org.esupportail.commons.services.logging.Logger;
 import org.esupportail.commons.services.logging.LoggerImpl;
-import org.esupportail.commons.utils.Assert;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.ldap.LdapTemplate;
+
 import org.springframework.ldap.UncategorizedLdapException;
 import org.springframework.ldap.support.DirContextAdapter;
 import org.springframework.ldap.support.DistinguishedName;
-import org.springframework.ldap.support.LdapContextSource;
 
 /**
  * An implementation of WriteableLdapService based on LdapTemplate.
  * See /properties/ldap/ldap-write-example.xml.
  */
-public class WriteableLdapUserServiceImpl implements WriteableLdapUserService, InitializingBean {
-	
+public class WriteableLdapUserServiceImpl extends org.esupportail.commons.services.ldap.WriteableLdapUserServiceImpl implements WriteableLdapUserService {
 	/**
 	 * The serialization id.
 	 */
 	private static final long serialVersionUID = -2833750508738328830L;
 
+	private CacheManager cacheManager;
+
 	/**
 	 * A logger.
 	 */
 	private final Logger logger = new LoggerImpl(getClass());
-
-	/**
-	 * A LdapTemplate instance, to perform LDAP operations.
-	 */
-	private LdapTemplate ldapTemplate;
-	
-	/**
-	 * A LdapContextSource instance, to modify LDAP connections.
-	 */
-	private LdapContextSource contextSource;
-
-	/**
-	 * The name of the attribute that contains the unique id.
-	 */
-	private String idAttribute;
-
-	/**
-	 * The DN of LDAP users. for example : ou=people,dc=domain,dc=edu
-	 */
-	private String dnAuth;
-	
-	/**
-	 * The name of the attribute that contains the unique id of LDAP users.
-	 */
-	private String idAuth;
-	
-	/**
-	 * The DN sub path.
-	 */
-	private String dnSubPath;
-	
-	/**
-	 * The names of the attributes to update.
-	 */
-	private List<String> attributes;
-	
-	private CacheManager cacheManager;
-
 	
 	/**
 	 * Bean constructor.
@@ -83,27 +43,6 @@ public class WriteableLdapUserServiceImpl implements WriteableLdapUserService, I
 		super();
 	}
 
-	/**
-	 * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	public void afterPropertiesSet() {
-		Assert.hasText(idAttribute, 
-				"property idAttribute of class " + getClass().getName() + " can not be null");
-		Assert.notEmpty(attributes,  
-				"property attributes of class " + getClass().getName() + " can not be empty");
-		Assert.hasText(dnSubPath,  
-				"property dnSubPath of class " + getClass().getName() + " can not be empty");
-		if (ldapTemplate == null) {
-			dnSubPath = null;
-			logger.info(getClass() + ": property ldapTemplate is not set"); 
-		}
-		if (logger.isDebugEnabled() && dnAuth != null) {
-			logger.debug("dnAuth" + dnAuth); 
-		}
-		if (!attributes.contains(idAttribute)) {
-			attributes.add(idAttribute);
-		}
-	}
 
 	/** Modify an LDAP user using Spring LdapContextSource.
 	 * @see org.esupportail.commons.services.ldap.WriteableLdapUserService#updateLdapUser(
@@ -111,69 +50,10 @@ public class WriteableLdapUserServiceImpl implements WriteableLdapUserService, I
 	 */
 	public void updateLdapUser(final LdapUser ldapUser) throws LdapAttributesModificationException {
 		
-		Name dn = buildLdapUserDn(ldapUser.getId());
-		
-		try {
-			if (logger.isTraceEnabled()) {
-				logger.trace("Looking for :" + dn);
-			}
-			
-			DirContextAdapter context = (DirContextAdapter) ldapTemplate.lookup(dn);
-			if (logger.isTraceEnabled()) {
-				logger.trace("mapToContext()" + ldapUser);
-			}
-			
-			mapToContext(ldapUser, context);
-			logger.info("Update of LDAP user :" + dn + " : " + ldapUser);
-			
-			ldapTemplate.modifyAttributes(dn, context.getModificationItems());
-			
-		} catch (UncategorizedLdapException e) {
-			if (e.getCause() instanceof javax.naming.AuthenticationException) {
-				throw new LdapBindFailedException("Couldn't bind to LDAP with user" + ldapUser.getId());
-			}
-		} catch (Exception e) {
-			if (logger.isTraceEnabled()) {
-				logger.trace("Error in updateLdapUser(): ", e);
-			}
-			throw new LdapAttributesModificationException(
-					"Couldn't get modification items for '" + dn + "'", e);
-		}
+		super.updateLdapUser(ldapUser);
 		invalidateLdapCache();		
 	}
-	
-	/** Create an LDAP user using Spring LdapContextSource.
-	 * @see org.esupportail.commons.services.ldap.WriteableLdapUserService#createLdapUser(
-	 * org.esupportail.commons.services.ldap.LdapUser)
-	 */
-	public void createLdapUser(final LdapUser ldapUser) {
-		Name dn = buildLdapUserDn(ldapUser.getId());
-		DirContextAdapter context = new DirContextAdapter(dn);
-		mapToContext(ldapUser, context);
-		ldapTemplate.bind(dn, context, null);
-		logger.info("created [" + dn + "] from [" + ldapUser + "]");
-	}
-	
-	/** Build user full DN.
-	 * @param userId
-	 * @return user full DN
-	 */
-	protected DistinguishedName buildLdapUserDn(final String userId) {
-		DistinguishedName dn;
-		dn = new DistinguishedName(dnSubPath);
-		dn.add(this.idAttribute, userId);
-		return dn;
-	}
-	
-	/** Delete an LDAP user using Spring LdapContextSource.
-	 * @see org.esupportail.commons.services.ldap.WriteableLdapUserService#deleteLdapUser(
-	 * org.esupportail.commons.services.ldap.LdapUser)
-	 */
-	public void deleteLdapUser(final LdapUser ldapUser) {
-		DistinguishedName ldapUserDn = buildLdapUserDn(ldapUser.getId());
-		ldapTemplate.unbind(ldapUserDn);
-	}
-	
+
 	/**
 	 * @param ldapUser
 	 * @param context
@@ -194,26 +74,11 @@ public class WriteableLdapUserServiceImpl implements WriteableLdapUserService, I
 		}
 	}
 	
-	/**
-	 * @see org.esupportail.commons.services.ldap.WriteableLdapUserService#setAuthenticatedContext(
-	 * java.lang.String, java.lang.String)
-	 */
-	public void setAuthenticatedContext(
-			final String userId, 
-			final String password) throws LdapException {
-		DistinguishedName ldapBindUserDn = new DistinguishedName(this.dnAuth);
-		ldapBindUserDn.add(this.idAuth, userId);
-		if (logger.isDebugEnabled()) {
-			logger.debug("Binding to LDAP with DN [" + ldapBindUserDn + "] (password ******)");
-		}
-		contextSource.setUserName(ldapBindUserDn.encode());
-		contextSource.setPassword(password);
-	}
-	
+
 	public void bindLdap(final LdapUser ldapUser)throws AuthentificationException{
 		try{
 			Name dn = buildLdapUserDn(ldapUser.getId());
-			ldapTemplate.lookup(dn);
+			this.getLdapTemplate().lookup(dn);
 		
 		} catch (UncategorizedLdapException e) {
 			logger.debug("Une authentification a échouée : "+e);
@@ -224,126 +89,27 @@ public class WriteableLdapUserServiceImpl implements WriteableLdapUserService, I
 	}
 	
 	public void defineAuthenticatedContext(String username, String password) throws LdapException {
-		contextSource.setUserName(username);
-		contextSource.setPassword(password);
+		this.getContextSource().setUserName(username);
+		this.getContextSource().setPassword(password);
 	}
 	
 	public void defineAuthenticatedContextForUser(String userId, String password) throws LdapException{
-		DistinguishedName ldapBindUserDn = new DistinguishedName(this.dnAuth);
-		ldapBindUserDn.add(this.idAuth, userId);
+		DistinguishedName ldapBindUserDn = new DistinguishedName(this.getDnAuth());
+		ldapBindUserDn.add(this.getIdAuth(), userId);
 		logger.debug("Binding to LDAP with DN [" + ldapBindUserDn + "] (password ******)");
 		
-		contextSource.setUserName(ldapBindUserDn.encode());
-		contextSource.setPassword(password);
+		this.getContextSource().setUserName(ldapBindUserDn.encode());
+		this.getContextSource().setPassword(password);
 	}
 
 	/**
 	 * @see org.esupportail.commons.services.ldap.WriteableLdapUserService#defineAnonymousContext()
 	 */
 	public void defineAnonymousContext() throws LdapException {
-		contextSource.setUserName("");
-		contextSource.setPassword("");
+		this.getContextSource().setUserName("");
+		this.getContextSource().setPassword("");
 	}
 
-	/**
-	 * @return ldapTemplate the LdapTemplate to get
-	 */
-	public LdapTemplate getLdapTemplate() {
-		return ldapTemplate;
-	}
-
-	
-	/**
-	 * @param ldapTemplate the LdapTemplate to set
-	 */
-	public void setLdapTemplate(LdapTemplate ldapTemplate) {
-		this.ldapTemplate = ldapTemplate;
-	}
-
-	/**
-	 * @return the contextSource to get
-	 */
-	public LdapContextSource getContextSource() {
-		return contextSource;
-	}
-
-	/**
-	 * @param contextSource the ContextSource to set
-	 */
-	public void setContextSource(LdapContextSource contextSource) {
-		this.contextSource = contextSource;
-	}
-
-	/**
-	 * @return the idAttribute to get
-	 */
-	public String getIdAttribute() {
-		return idAttribute;
-	}
-
-	/**
-	 * @param idAttribute the idAttribute to set
-	 */
-	public void setIdAttribute(final String idAttribute) {
-		this.idAttribute = idAttribute;
-	}
-
-	/**
-	 * @return the dnSubPath to get
-	 */
-	public String getDnSubPath() {
-		return dnSubPath;
-	}
-
-	/**
-	 * @param dnSubPath the dnSubPath to set
-	 */
-	public void setDnSubPath(final String dnSubPath) {
-		this.dnSubPath = dnSubPath;
-	}
-
-	/**
-	 * @return the attributes to get
-	 */
-	public List<String> getAttributes() {
-		return attributes;
-	}
-
-	/**
-	 * @param attributes the attributes to set
-	 */
-	public void setAttributes(final List<String> attributes) {
-		this.attributes = attributes;
-	}
-
-	/**
-	 * @return the dnAuth to get
-	 */
-	public String getDnAuth() {
-		return dnAuth;
-	}
-
-	/**
-	 * @param dnAuth the dnAuth to set
-	 */
-	public void setDnAuth(final String dnAuth) {
-		this.dnAuth = dnAuth;
-	}
-
-	/**
-	 * @return the idAuth to get
-	 */
-	public String getIdAuth() {
-		return idAuth;
-	}
-
-	/**
-	 * @param idAuth the idAuth to set
-	 */
-	public void setIdAuth(final String idAuth) {
-		this.idAuth = idAuth;
-	}
-	
 	public void invalidateLdapCache() {
 		net.sf.ehcache.Cache cache = cacheManager.getCache(org.esupportail.commons.services.ldap.CachingLdapEntityServiceImpl.class.getName());
 		cache.removeAll();		
