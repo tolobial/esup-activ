1a24f6317e196c111c3eb599c26c126d16dcc17c
csar <csar@df3935f2-5e34-0410-be39-dc869cc82e10>
2014-10-02 09:38:03 +0000

    Utilisation de bootstrap.min.css + portletContainer de esup bootstrap EQ (avec gestion de multi-colonnes...) à la place de bootstrap.css
    git-svn-id: https://subversion.renater.fr/esup-activ/branches/esup-activ-2.0@1423 df3935f2-5e34-0410-be39-dc869cc82e10

---
+++ esup-activ-fo/webapp/media/scripts/portletContainer.js
@@ -0,0 +1,33 @@
+(function($){
+
+    var $portletContainers;
+
+    $(document).ready(function() {
+        $portletContainers = $('.pc');
+        $(window).resize(onWindowResize);
+        onWindowResize();
+    });
+
+    function onWindowResize() {
+
+        $portletContainers.each(function(index) {       
+            var $that = $(this);
+            var portletWidth = $that.width();
+
+            $that.removeClass("xs sm md lg");
+            if(portletWidth < 768) { 
+                $that.addClass("xs"); 
+            }
+            if(portletWidth >= 768 && portletWidth < 992) { 
+                $that.addClass("sm"); 
+            }
+            if(portletWidth >= 992 && portletWidth < 1200) { 
+                $that.addClass("md"); 
+            }
+            if(portletWidth >= 1200) { 
+                $that.addClass("lg"); 
+            }   
+        });
+    }
+
+})(jQuery);
--- esup-activ-fo/webapp/stylesheets/_includeScript.jsp
+++ esup-activ-fo/webapp/stylesheets/_includeScript.jsp
@@ -3,8 +3,9 @@
 <script type="text/javascript" src="/media/scripts/jquery-ui-1.10.3.custom.js"></script>
 <script type="text/javascript" src="/media/scripts/form-show-hide.js"></script>
 <script type="text/javascript" src="/media/scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="/media/scripts/portletContainer.js"></script>
 
 
 <style type="text/css">@import url("/media/jquery-ui-1.10.3.custom.css");</style>
-<style type="text/css">@import url("/media/bootstrap.css");</style>
+<style type="text/css">@import url("/media/bootstrap.min.css");</style>
 <style type="text/css">@import url("/media/esupActivation.css");</style>
